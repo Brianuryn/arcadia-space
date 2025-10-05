883e4ce03c0b5ad669b86d502c72d283b91cea1c
Brianuryn <brianuryn2000@gmail.com>
2025-10-05 11:48:59 -0300

    Arreglo imagen background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "../styles/Home.css";
 import HabitatDesigner from '../components/HabitatDesigner/HabitatDesigner.jsx';
+import bg from "../resources/background.jpeg";
 
 function Home() {
   return (
-    <div className="home-container">
+    <div className="home-container" style={{backgroundImage: `url(${bg})`}}>
       <div className="home-content">
         <h1>Bienvenido a Arkadia Space 🚀</h1>
         <p>Explorando el futuro del hábitat espacial.</p>
